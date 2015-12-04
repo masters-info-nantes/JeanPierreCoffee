f3a0c284d86db495d80953955dc421198a28fcd3
nBourdin <nicolas.bourdin@mail.com>
2015-12-04 13:57:43 +0100

    works with a specific server (adrien+nicolas)

--- coffee_back.py
+++ coffee_back.py
@@ -1,20 +1,42 @@
-from flask import Flask, redirect, url_for
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, redirect, url_for, jsonify
+import json
 import requests
 app = Flask(__name__)
 
-AUTH_SERVER = "http://blank.org/"
-token = ""
+AUTH_SERVER = 'http://localhost:8080/ame-services-rest/service/token'
+LOG_SERVER = ''
 
-@app.route("/hello")
+@app.route('/hello', methods=['GET'])
 def hello():
-	#get request on authentication server
-	token = requests.get(AUTH_SERVER).content
-	print(token)
-	return ""
+	return jsonify({'url' : AUTH_SERVER})
 
-@app.route("/buyCoffee")
-def buyCoffee():
-	return 
+@app.route("/buycoffee/<token>", methods=['GET'])
+def buyCoffee(token, charset='utf-8'):
+	print token
+
+	logs = jsonify({
+		'actionType' : "drink",
+		'vendingMachineId' : "",
+		'vendingMachineType' : "",
+		'userType' : "",
+		'userId' : "",
+		'previousBalance' : 0.0,
+		'currentBalance' : 0.0,
+		'cost' : 0.0,
+		'itemId' : "",
+		'isoDate' : ""
+	})
+
+	headers = {'Api-Key': 'reQSFGgFSbgc54uyhjg35hgf23vJhg432JNkjH', 'Authorization': str(token), 'Content-Type': 'application/json;charset=UTF-8'}
+	r = requests.get(AUTH_SERVER, headers=headers)
+	print r	
+	#print r.content
+	#Send logs to log server
+	#l = requests.post(LOG_SERVER, logs=logs)
+	#print l.content
+	return (r.text, r.status_code, r.headers.items())
 
 if __name__ == "__main__":
     app.run(debug=True)
